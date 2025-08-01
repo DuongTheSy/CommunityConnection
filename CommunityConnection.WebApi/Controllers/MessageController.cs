a93529f32b927859b1c715882809c69cc0a4e86e
duongthesy <duongthesy03@gmail.com>
2025-08-01 16:04:25 +0700

    .

--- CommunityConnection.WebApi/Controllers/MessageController.cs
+++ CommunityConnection.WebApi/Controllers/MessageController.cs
@@ -55,43 +55,43 @@ namespace CommunityConnection.WebApi.Controllers
             var result = await _service.GetMessagesAsync(userId,communityId, channelId);
             return Ok(result);
         }
-        [HttpPost]
-        [Route("communities/channels/messages")]
-        public async Task<IActionResult> SendMessage(long channelId, [FromBody] MessageCreateDto dto)
-        {
-            if (!User.Identity?.IsAuthenticated ?? false)
-            {
-                return Unauthorized(new ApiResponse<StatusResponse>
-                {
-                    status = true,
-                    message = "Thành công",
-                    data = new StatusResponse
-                    {
-                        status = false,
-                        message = "Bạn cần đăng nhập"
+        //[HttpPost]
+        //[Route("communities/channels/messages")]
+        //public async Task<IActionResult> SendMessage(long channelId, [FromBody] MessageCreateDto dto)
+        //{
+        //    if (!User.Identity?.IsAuthenticated ?? false)
+        //    {
+        //        return Unauthorized(new ApiResponse<StatusResponse>
+        //        {
+        //            status = true,
+        //            message = "Thành công",
+        //            data = new StatusResponse
+        //            {
+        //                status = false,
+        //                message = "Bạn cần đăng nhập"
 
-                    }
-                });
-            }
+        //            }
+        //        });
+        //    }
 
-            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
-            if (userIdClaim == null)
-                return Unauthorized(new ApiResponse<StatusResponse>
-                {
-                    status = true,
-                    message = "Thành công",
-                    data = new StatusResponse
-                    {
-                        status = false,
-                        message = "Kiểm tra lại Token"
+        //    var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        //    if (userIdClaim == null)
+        //        return Unauthorized(new ApiResponse<StatusResponse>
+        //        {
+        //            status = true,
+        //            message = "Thành công",
+        //            data = new StatusResponse
+        //            {
+        //                status = false,
+        //                message = "Kiểm tra lại Token"
 
-                    }
-                });
+        //            }
+        //        });
 
-            long userId = long.Parse(userIdClaim.Value);
+        //    long userId = long.Parse(userIdClaim.Value);
 
-            var result = await _service.SendMessageAsync(channelId, userId, dto);
-            return Ok(result);
-        }
+        //    var result = await _service.SendMessageAsync(channelId, userId, dto);
+        //    return Ok(result);
+        //}
     }
 }
